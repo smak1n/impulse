2cae715d5b1f9d5a9ec808867e508683c398665a
Ismail <vinux181@gmail.com>
2023-11-28 16:16:38 +0100

    initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>Hello Impulse</div>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* {public routes} */}
+        <Route path="sign-in" element={<SigninForm />} />
+        <Route path="sign-up" element={<SignupForm />} />
+
+        {/* {private routes} */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   )
 }
 

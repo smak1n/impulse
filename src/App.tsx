0af5d7f0492f4f4a44b6d6caa4e0494915cd5f7e
Ismail <vinux181@gmail.com>
2023-11-28 15:43:15 +0100

    init-project+tailwindcss configs

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './globals.css';
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline'>Hello Impulse</div>
+  )
+}
+
+export default App

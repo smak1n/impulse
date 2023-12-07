e936e304663ca8634c8fc6bf3fba26292a125a6a
Ismail <vinux181@gmail.com>
2023-12-07 15:12:04 +0100

    fixing typo

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -5,15 +5,15 @@ export const appwriteConfig = {
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
   storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
-  userCollectionId: import.meta.env.VITEAPPWRITE_USER_COLLECTION_ID,
-  postCollectionId: import.meta.env.VITEAPPWRITE_POST_COLLETION_ID,
-  savesCollectionId: import.meta.env.VITEAPPWRITE_SAVES_COLLECTION_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLETION_ID,
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
 }
 
 export const client = new Client();
 
+client.setEndpoint(appwriteConfig.url);
 client.setProject(appwriteConfig.projectId);
-client.setEndpoint(appwriteConfig.url)
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
